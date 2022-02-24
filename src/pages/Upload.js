f6c11946770b62fc02932067d83d5e92eee7daa2
softwat <ghr95223@gmail.com>
2022-02-24 14:51:04 +0900

    [모든 기능 완료]

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import apis from '../api/api';
 
-import Template from '../common/Template';
+import TemplateRyu from '../common/TemplateRyu';
 
 const Upload = props => {
     const [musicFile, setMusicFile] = useState(null);
@@ -15,15 +15,15 @@ const Upload = props => {
     const [imgFile, setImgFile] = useState(null);
 
     const handleMusicFile = event => {
-        console.log(event.target.files);
+        if (!event.target.files) window.alert('음악파일을 선택해주세요');
         setMusicFile(event.target.files);
     };
 
     const handleImageFile = event => {
         console.log(event.target.files);
         setImgFile(event.target.files);
-        //fd.append("file", event.target.files)
         setImgBase64([]);
+
         for (var i = 0; i < event.target.files.length; i++) {
             if (event.target.files[i]) {
                 let reader = new FileReader();
@@ -34,13 +34,10 @@ const Upload = props => {
                     const base64 = reader.result;
                     console.log(base64);
                     if (base64) {
-                        //  images.push(base64.toString())
                         var base64Sub = base64.toString();
 
                         setImgBase64(imgBase64 => [...imgBase64, base64Sub]);
-                        //  setImgBase64(newObj);
                         // 파일 base64 상태 업데이트
-                        //  console.log(images)
                     }
                 };
             }
@@ -50,10 +47,20 @@ const Upload = props => {
     const WriteBoard = async () => {
         const fd = new FormData();
 
-        fd.append('music', musicFile[0]);
-        fd.append('musicTitle', title.current.value);
-        fd.append('artistName', artist.current.value);
-        fd.append('musicCategory', category.current.value);
+        const music = musicFile[0];
+        const musicTitle = title.current.value;
+        const artistName = artist.current.value;
+        const musicCategory = category.current.value;
+
+        if (!music || !musicTitle || !artistName || !musicCategory) {
+            window.alert('모든 필드를 입력해야 합니다');
+        }
+
+        fd.append('music', music);
+        fd.append('musicTitle', musicTitle);
+        fd.append('artistName', artistName);
+        fd.append('musicCategory', musicCategory);
+
         Object.values(imgFile).forEach(file => fd.append('image', file));
 
         // FormData의 key 확인
@@ -76,63 +83,91 @@ const Upload = props => {
                 if (res.data) {
                     console.log(res.data);
                 }
+                props.history.replace('/');
             })
             .catch(err => console.log(err));
     };
 
     return (
-        <Template>
-            <FormContainer>
-                <label htmlFor="multipartFile">음악</label>
-                <input
-                    type="file"
-                    id="music"
-                    onChange={handleMusicFile}
-                    multiple="multiple"
-                    name="multipartFile"
-                />
-                <label htmlFor="image">이미지</label>
-                <input
-                    type="file"
-                    id="image"
-                    onChange={handleImageFile}
-                    multiple="multiple"
-                    name="image"
-                />
-                <input name="text" id="title" placeholder="Title" ref={title} />
-                <input
-                    name="text"
-                    id="artist"
-                    placeholder="artist"
-                    ref={artist}
-                />
-                <input
-                    name="text"
-                    id="category"
-                    placeholder="category"
-                    ref={category}
-                />
-                <button
-                    onClick={WriteBoard}
-                    style={{
-                        border: '2px solid black',
-                        width: '700px',
-                        fontSize: '40px',
-                    }}
-                >
-                    {imgBase64.map(item => {
-                        return (
-                            <img
-                                src={item}
-                                style={{ width: '100%', height: 'auto' }}
-                                alt="미리보기"
+        <TemplateRyu>
+            <div style={{ height: '90px' }}></div>
+            <UploadBox>
+                {!musicFile ? (
+                    <>
+                        <TitleBtnContainer>
+                            <h1
+                                style={{
+                                    fontSize: '22px',
+                                    fontFamily: 'Interstate',
+                                    fontWeight: '350',
+                                }}
+                            >
+                                Drag and drop your tracks & albums here
+                            </h1>
+                            <BtnMusicUpload htmlFor="music">
+                                or choose files to upload
+                            </BtnMusicUpload>
+                            <input
+                                style={{ display: 'none' }}
+                                type="file"
+                                id="music"
+                                onChange={handleMusicFile}
+                                multiple="multiple"
+                                name="multipartFile"
                             />
-                        );
-                    })}
-                    작성완료
-                </button>
-            </FormContainer>
-        </Template>
+                        </TitleBtnContainer>
+                        <aside style={{ fontSize: '12px' }}>
+                            Provide FLAC, WAV, ALAC, or AIFF for highest audio
+                            quality.
+                        </aside>{' '}
+                    </>
+                ) : (
+                    <>
+                        <InfoContainer>
+                            <div style={{ position: 'relative' }}>
+                                <ImageUploader htmlFor="image">
+                                    Upload image
+                                </ImageUploader>
+                                <input
+                                    style={{ display: 'none' }}
+                                    type="file"
+                                    id="image"
+                                    onChange={handleImageFile}
+                                    multiple="multiple"
+                                    name="image"
+                                />
+                                <ImgPre src={imgBase64[0]}></ImgPre>
+                            </div>
+                            <InputWrapper>
+                                <InputContainer>
+                                    <span>Title</span>
+                                    <TextInput ref={title}></TextInput>
+                                </InputContainer>
+                                <InputContainer>
+                                    <span>artist</span>
+                                    <TextInput ref={artist}></TextInput>
+                                </InputContainer>
+                                <InputContainer>
+                                    <span>category</span>
+                                    <Category ref={category}>
+                                        <option value={'rock'}>rock</option>
+                                        <option value={'hiphop'}>hiphop</option>
+                                    </Category>
+                                </InputContainer>
+                            </InputWrapper>
+                        </InfoContainer>
+                        <SubmitBox>
+                            <BtnCancle
+                                onClick={() => props.history.replace('/')}
+                            >
+                                Cancle
+                            </BtnCancle>
+                            <BtnSave onClick={WriteBoard}>Save</BtnSave>
+                        </SubmitBox>
+                    </>
+                )}
+            </UploadBox>
+        </TemplateRyu>
     );
 };
 
@@ -143,8 +178,128 @@ const Upload = props => {
 // musicCategory : ${음악카테고리}//
 // imageUrl : ${이미지파일}
 
-const FormContainer = styled.div`
-    margin-top: 70px;
+const InfoContainer = styled.div`
+    display: flex;
+    padding: 14px 25px 0;
+`;
+
+const ImageUploader = styled.label`
+    cursor: pointer;
+    position: absolute;
+    bottom: 10%;
+    padding: 2px 11px 2px 10px;
+    font-size: 14px;
+    left: 30%;
+    color: #333;
+    border-color: rgba(0, 0, 0, 0.3);
+    background-color: #e5e5e5;
+    border-radius: 3px;
+`;
+
+const ImgPre = styled.img`
+    width: 260px;
+    height: 260px;
+    background-color: black;
+`;
+
+const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex: 1;
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 20px;
+`;
+
+const TextInput = styled.input`
+    width: 100%;
+    padding: 2px 7px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    transition: border-color 0.1s;
+`;
+
+const Category = styled.select`
+    padding: 2px 7px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    transition: border-color 0.1s;
+`;
+
+const SubmitBox = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    border-top: 1px solid #f2f2f2;
+    margin-top: 20px;
+    padding: 20px;
+`;
+
+const UploadBox = styled.div`
+    border: 1px solid #f2f2f2;
+    border-radius: 2px;
+    margin: -30px auto 0;
+    padding: 30px 0 0 0;
+    width: 800px;
+    box-shadow: 0 2px 12px -5px rgba(0, 0, 0, 0.1);
+    text-align: center;
+`;
+
+const BtnCancle = styled.button`
+    margin-right: 20px;
+    height: 26px;
+    font-size: 14px;
+    line-height: 20px;
+    white-space: nowrap;
+    font-family: Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans,
+        Garuda, Verdana, Tahoma, sans-serif;
+    font-weight: 100;
+    text-align: center;
+    vertical-align: baseline;
+`;
+
+const BtnSave = styled.button`
+    height: 26px;
+    padding: 2px 11px 2px 10px;
+    border: 1px solid #e5e5e5;
+    border-radius: 3px;
+    font-size: 14px;
+    line-height: 20px;
+    white-space: nowrap;
+    font-family: Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans,
+        Garuda, Verdana, Tahoma, sans-serif;
+    font-weight: 100;
+    text-align: center;
+    vertical-align: baseline;
+    background-color: #f50;
+    border-color: #f50;
+    color: #fff;
+`;
+
+const TitleBtnContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px 0;
+`;
+
+const BtnMusicUpload = styled.label`
+    cursor: pointer;
+    display: block;
+    margin: 16px 0 12px;
+    width: 300px;
+    background-color: #f50;
+    border-color: #f50;
+    color: #fff;
+    font-size: 16px;
+    line-height: 18px;
+    padding: 10px 15px;
+    height: 40px;
+    border-radius: 3px;
 `;
 
 export default Upload;
